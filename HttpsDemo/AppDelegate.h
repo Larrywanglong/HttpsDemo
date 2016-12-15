458d58b17eabcfb63ae2072ccf97460edd43cb2b
Larrywanglong <wanglong@wanglong.local>
2016-12-15 09:51:27 +0800

    Initial Commit

---
+++ HttpsDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HttpsDemo
+//
+//  Created by 王龙 on 16/12/15.
+//  Copyright © 2016年 王龙. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
